96b7b537d61afe90f9275d9ea9be777a55d0af89
abduIIahKhaIid <abdullahakhalid6@gmail.com>
2024-03-12 16:40:21 +0500

    first commit

--- src/app/favourite/page.tsx
+++ src/app/favourite/page.tsx
@@ -1,28 +1,24 @@
-"use server"
+
 import CloudinaryImage from '@/app/gallery/cloudinary-image';
 import UploadButton from '@/app/gallery/uploading-button';
 import cloudinary from "cloudinary";
+import { SearchResult } from '../gallery/page';
 
-export type SearchResult = {
-    public_id: string;
-    tags: string[];
-};
 
-export default async function Gallery() {
+export default async function FavoritesPage() {
     const results = (await cloudinary.v2.search
-        .expression('resource_type:image ')
-        .sort_by('created_at', 'desc')
-        .with_field('tags')
-        .max_results(1)
+        .expression("resource_type:image AND tags=favorite")
+        .sort_by("created_at", "desc")
+        .with_field("tags")
+        .max_results(30)
         .execute()) as { resources: SearchResult[] };
     return (
         <section>
             <div className="flex flex-col gap-8">
                 <div className='flex justify-between'>
                     <h1 className="text-4xl font-bold">
-                        Gallery
+                        Favourite Images
                     </h1>
-                    <UploadButton />
                 </div>
                 <div className='grid grid-cols-4 gap-5'>
                     {results.resources.map((result) => (
